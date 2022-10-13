c1065c336b8fac58069365e74fd231bde94d34bf
Jennifer Alexander <110498167+jsalexan@users.noreply.github.com>
2022-10-13 14:01:37 -0700

    Starting to really flesh out the sample markdown and have wrestled through updating the question prompts to sync with it. I'd like to try running this soon to see how it's coming together, then I need to figure out the license and badge formulas, along with the function to add text if there are no contributors.

--- Develop/index.js
+++ Develop/index.js
@@ -6,18 +6,39 @@ inquirer
   .prompt([
     {
       type: 'input',
+      name: 'name',
+      message: 'What is your full name?'
+    },
+    {
+      type: 'input',
       name: 'title',
       message: 'What is your project title?',
     },
     {
-      type: 'input',
-      name: 'description',
-      message: 'Please enter a description of your project.', 
+      type: 'rawlist',
+      name: 'license',
+      message: 'Please choose your license for this application.',
+      choices: ['MIT', 'Apache 2.0', 'GPL', 'GBD', 'BSD','ISC']
     },
     {
       type: 'input',
-      name: 'TOC',
-      message: 'Please enter your table of contents.',
+      name: 'descr-what',
+      message: 'Please describe what your application does.', 
+    },
+    {
+      type: 'input',
+      name: 'descr-tech',
+      message: 'What technologies did you use for this project?'
+    },
+    {
+      type: 'input',
+      name: 'descr-challenges',
+      message: 'What are some of the challenges you faced?'
+    },
+    {
+      type: 'input',
+      name: 'descr-future',
+      message: 'What features would you like to add to the application in the future?'
     },
     {
       type: 'input',
@@ -27,18 +48,54 @@ inquirer
     { 
       type: 'input',
       name: 'Usage',
-      message: 'Please describe how users will use your project.',
+      message: 'Please provide instructions and/or examples so users/contributors can use the project.',
+    },
+    { 
+      type: 'input',
+      name: 'collaborators',
+      message: 'Please enter information about your collaborators on this application.',
+    },
+    { 
+      type: 'input',
+      name: 'links',
+      message: 'If you used any third party tutorials or links, please enter the list of links here. Please separate each link with a comma.',
     },
     { 
       type: 'input',
       name: 'contribution',
-      message: 'Please enter your contribution guidelines.',
+      message: 'Please enter your contribution guidelines. If you do not wish to have contributors, please skip this question.'
         },
     { 
       type: 'input',
-      name: 'contribution',
-      message: 'Please enter your contribution guidelines.',
-              }
+      name: 'tests',
+      message: 'Please enter any tests you have written for this application.',
+    },
+    { 
+      type: 'input',
+      name: 'GHusername',
+      message: 'Please enter your GitHub username.',
+    },
+    { 
+      type: 'input',
+      name: 'url',
+      message: 'Please enter the URL for your application.',
+    },
+    { 
+      type: 'input',
+      name: 'repo-name',
+      message: 'Please enter the GitHub repository name for this application.',
+    },
+    { 
+      type: 'checkbox',
+      name: 'file-names',
+      message: 'Which of the following file types are included in your repository?',
+      choices: ['HTML', 'CSS', 'JavaScript', 'JSON', 'README', 'Images']
+    },
+    { 
+      type: 'input',
+      name: 'img-path',
+      message: 'If you would like to add a screenshot, please enter the pathway to the image file here.',
+    },
   ])
 
 // TODO: Create a function to write README file
--- Develop/sample.md
+++ Develop/sample.md
@@ -1,9 +1,17 @@
 # ${data.title}
 
+## [Badges](#table-of-contents)
+![${data.license}](https://img.shields.io/github/license/jsalexan/congenial-journey)
+<br><br>
 ## Description
-${data.descr}
-
-
+${data.descr-what}
+<br>
+${data.descr-tech}
+<br>
+${data.descr-challenges}
+<br>
+${data.descr-future}
+<br><br>
 ## Table of Contents
 - [Installation](#installation)
 - [Usage](#usage)
@@ -13,48 +21,35 @@ ${data.descr}
 - [How to Contribute](#how-to-contribute)
 - [Tests](#tests)
 - [Questions](#questions)
-
-## Installation
+<br><br>
+## [Installation](#table-of-contents)
 The website is hosted by GitHub pages. <br>
 You can find it here: [Link to ${data.title}](${data.url}/)
 <br><br>
 The repository is on GitHub: [Link to GitHub repository for ${data.title}.](https://github.com/${data.GHusername}/{data.repo-name}) <br>
-There you will find following : 
+There you will find following file types : 
 ${data.file-names}
-<br>
-
-## Usage
-The website (link above) was created to be used on ${data.devices}.
-![Screen capture.](${data.img-path})
-
-
-<br>
-
-## Credits
+<br><br>
+## [Usage](#table-of-contents)
+The website (link above) was created to be used on \${data.devices}.
+![Screen capture.](\${data.img-path})
+<br><br>
+## [Credits](#table-of-contents) 
 ${data.collaborators}
-
+<br>
 **Third Party References and Tutorials used include:** 
 <br>
 > -${data.links}
-
-<br>
-## License
-
-
+<br><br>
+## [License](#table-of-contents)
 Copyright ${data.year}, ${data.name}
-
-
-
-<br>
-
-## Badges
-![${data.license}](https://img.shields.io/github/license/jsalexan/congenial-journey)
-
-## How to Contribute
+<br><br>
+## [How to Contribute](#table-of-contents)
 ${data.contribute
 }
-## Tests
+<br><br>
+## [Tests](#table-of-contents)
 ${data.tests}
-<br>
-## Questions
+<br><br>
+## [Questions](#table-of-contents)
 If you have any questions or comments, you can contact ${data.name} at ${data.email} or visit my GitHub at (https://github.com/${data.GHusername}/)
